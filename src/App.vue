274c21e5c2d99f6239da4b4d3cb415a74c6bdb68
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2022-01-12 11:56:59 +0400

    alive component

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
   <button @click="activeTab = 'TabA'">Tab A</button>
   <button @click="activeTab = 'TabB'">Tab B</button>
   <button @click="activeTab = 'TabC'">Tab C</button>
-
+<keep-alive>
 <component :is="activeTab"/>
-
+</keep-alive>
   <!-- <TabA v-if="activeTab === 'TabA'"/>
   <TabB v-if="activeTab === 'TabB'"/>
   <TabC v-if="activeTab === 'TabC'"/>
--- src/components/TabC.vue
+++ src/components/TabC.vue
@@ -1,10 +1,16 @@
 <template>
   <div>Tab C Content</div>
+  <input type="text" v-model="name">
 </template>
 
 <script>
 export default {
     name: 'TabC',
+    data(){
+        return{
+            name: ''
+        }
+    }
 }
 </script>
 
